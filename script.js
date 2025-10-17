06fc11bbe800a87aa1e8d61994dd4186593fe367
Amin Abedi <hifontamin@gmail.com>
2025-10-17 19:18:20 +0330

    Testing Page Redesigned
    Testing Page Redesigned to be fully Responsive with better UI and more clever UX

---
+++ script.js
@@ -0,0 +1,103 @@
+var curSize = null; // localStorage.getItem("saveSize");
+var slider = document.querySelector('.font-slider input[type="range"]')
+var body = document.body
+var defaultSize = body.style.getPropertyValue('font-size');
+
+if (curSize) {
+  var saveSize = curSize;
+  document.documentElement.style.setProperty("--font-size", saveSize + "px");
+  //body.style.fontSize = saveSize + 'px';
+}
+
+slider.addEventListener("input", function(e) {
+  var newSize = this.value,
+    minSize = 10,
+    maxSize = 72;
+
+  if (newSize <= maxSize && newSize >= minSize) {
+    document.documentElement.style.setProperty("--font-size", newSize + "px");
+    //    body.style.fontSize = newSize + 'px';
+    //    localStorage.setItem("saveSize", newSize);
+  }
+});
+
+const pageMain = document.querySelector(".dynamic");
+
+let __wghtVal = 400;
+let __kshdVal = 100;
+
+const wghtInput = document.getElementById("wght");
+const wghtSpan = document.getElementById("wghtVal");
+
+const kshdInput = document.getElementById("KSHD");
+const kshdSpan = document.getElementById("kshdVal");
+
+const Trigger = new Event("changeVal");
+
+function wghtVal(val = undefined) {
+  if (val) {
+    __wghtVal = val;
+  }
+  return `"wght" ${__wghtVal}`;
+}
+
+function kshdVal(val = undefined) {
+  if (val) {
+    __kshdVal = val;
+  }
+  return `"KSHD" ${__kshdVal}`;
+}
+
+const content = document.querySelector(".dynamic");
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  content.dispatchEvent(new Event("changeVal"));
+});
+
+content.addEventListener("changeVal", () => {
+  content.setAttribute(
+    "style",
+    `font-variation-settings: ${wghtVal()},${kshdVal()};`
+  );
+});
+
+wghtInput.addEventListener(".font-slider", (e) => {
+  wghtVal(e.target.value);
+  wghtSpan.innerText = __wghtVal;
+  content.dispatchEvent(Trigger);
+});
+
+kshdInput.addEventListener(".font-slider", (e) => {
+  kshdVal(e.target.value);
+  kshdSpan.innerText = __kshdVal;
+  content.dispatchEvent(Trigger);
+});
+
+function dice() {
+  wghtVal(Math.floor(Math.random() * 900) + 100);
+  kshdVal(Math.floor(Math.random() * 100) + 30);
+
+  wghtInput.value = __wghtVal;
+  kshdInput.value = __kshdVal;
+  wghtInput.dispatchEvent(new Event("input"));
+  kshdInput.dispatchEvent(new Event("input"));
+}
+
+document.getElementById("randbutton").addEventListener("click", dice);
+
+function copyElementText(id) {
+    var text = document.getElementById(id).innerText;
+    var elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = text;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+}
+function ShowSecond() {
+    var div2 = document.getElementById("div2");
+    div2.className = "show";
+    setTimeout(function () {
+        div2.className = 'hide';
+    }, 2000);
+}
